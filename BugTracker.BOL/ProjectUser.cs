59ae251c1fa814a4bc4be2f0ce8d130d6a85d783
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-06-07 00:05:29 +0500

    Implement database (#1)

---
+++ BugTracker.BOL/ProjectUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.BOL
+{
+
+    [Table("ProjectUser")]
+    public class ProjectUser : BaseEntity
+    {
+        public Guid ProjectId { get; set; }// foreign key
+        public Guid ProjectRolesId { get; set; }// foreign key
+        public Guid UserId { get; set; }
+
+        //Navigations
+        [ForeignKey("ProjectId")]
+        public virtual Projects? Projects { get; set; }
+
+        [ForeignKey("ProjectRolesId")]
+        public virtual ProjectRoles? ProjectRoles { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual OrganizationUsers? OrganizationUsers { get; set; }
+        public virtual IEnumerable<Tasks> Tasks { get; set; }
+        public virtual IEnumerable<TaskDetail> TaskDetail { get; set; }
+        public ProjectUser()
+        {
+
+        }
+
+        public ProjectUser(Guid projectId, Guid projectRolesId, Guid userId)
+        {
+            ProjectId = projectId;
+            ProjectRolesId = projectRolesId;
+            UserId = userId;
+        }
+    }
+}
+
+
---
+++ BugTracker.DAL/Data/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using BugTracker.BOL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.DAL.Data
+{
+   public class AppDbContext:DbContext
+   {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=MEHROZQAZI-PC\SQLEXPRESS;Database=BugTracker;Trusted_Connection=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder == null)
+                throw new ArgumentNullException("modelBuilder");
+
+            // for the other conventions, we do a metadata model loop
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                // equivalent of modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+                entityType.SetTableName(entityType.DisplayName());
+
+                // equivalent of modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+                entityType.GetForeignKeys()
+                    .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+                    .ToList()
+                    .ForEach(fk => fk.DeleteBehavior = DeleteBehavior.Restrict);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Db Set
+        public DbSet<Organizations> Organizations { get; set; }
+        public DbSet<OrganizationRoles> OrganizationRoles { get; set; }
+        public DbSet<OrganizationUsers> OrganizationUsers { get; set; }
+        public DbSet<Projects> Projects{ get; set; }
+        public DbSet<ProjectRoles> ProjectRoles { get; set; }
+        public DbSet<ProjectUser> ProjectUser { get; set; }
+
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<TaskDetail> TaskDetail { get; set; }
+
+    }
+
+}
