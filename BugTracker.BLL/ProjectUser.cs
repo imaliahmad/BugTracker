319e0080b3062bf56e9bb70511748e8415d726f7
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-06-08 00:04:41 +0500

    Implement BLL Layer (#3)

---
+++ BugTracker.BLL/ProjectUser.cs
@@ -0,0 +1,48 @@
+﻿using BugTracker.BOL;
+using BugTracker.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.BLL
+{
+    public interface IProjectUserBs
+    {
+        IEnumerable<ProjectUser> GetAll();
+        ProjectUser GetById(int id);
+        bool Insert(ProjectUser obj);
+        bool Update(ProjectUser obj);
+        bool Delete(int id);
+    }
+    public class ProjectUserBs : IProjectUserBs
+    {
+        private readonly IProjectUserDb objDb;
+
+        public ProjectUserBs(IProjectUserDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<ProjectUser> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public ProjectUser GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(ProjectUser obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(ProjectUser obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
---
+++ BugTracker.BLL/TaskDetail.cs
@@ -0,0 +1,48 @@
+﻿using BugTracker.BOL;
+using BugTracker.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.BLL
+{
+    public interface ITaskDetailBs
+    {
+        IEnumerable<TaskDetail> GetAll();
+        TaskDetail GetById(int id);
+        bool Insert(TaskDetail obj);
+        bool Update(TaskDetail obj);
+        bool Delete(int id);
+    }
+    public class TaskDetailBs : ITaskDetailBs
+    {
+        private readonly ITaskDetailDb objDb;
+
+        public TaskDetailBs(ITaskDetailDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<TaskDetail> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public TaskDetail GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(TaskDetail obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(TaskDetail obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
