f3853c7426788d992eae3b65549d0f43529604d6
MehrozQazi <mehrozqazi346@gmail.com>
2023-06-07 23:42:09 +0500

    Implement DAL Layer

---
+++ BugTracker.DAL/ProjectsDb.cs
@@ -0,0 +1,55 @@
+﻿using BugTracker.BOL;
+using BugTracker.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.DAL
+{
+    public interface IProjectsDb
+    {
+        IEnumerable<Projects> GetAll();
+        Projects GetById(int id);
+        bool Insert(Projects obj);
+        bool Update(Projects obj);
+        bool Delete(int id);
+    }
+    public class ProjectsDb : IProjectsDb
+    {
+        private AppDbContext context;
+        public ProjectsDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Projects> GetAll()
+        {
+            return context.Projects.ToList();
+        }
+        public Projects GetById(int id)
+        {
+            var obj = context.Projects.Find(id);
+            return obj;
+        }
+        public bool Insert(Projects obj)
+        {
+            context.Projects.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(Projects obj)
+        {
+            context.Projects.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.Projects.Find(id);
+            context.Projects.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ BugTracker.DAL/TaskDetailDb.cs
@@ -0,0 +1,55 @@
+﻿using BugTracker.BOL;
+using BugTracker.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.DAL
+{
+    public interface ITaskDetailDb
+    {
+        IEnumerable<TaskDetail> GetAll();
+        TaskDetail GetById(int id);
+        bool Insert(TaskDetail obj);
+        bool Update(TaskDetail obj);
+        bool Delete(int id);
+    }
+    public class TaskDetailDb : ITaskDetailDb
+    {
+        private AppDbContext context;
+        public TaskDetailDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<TaskDetail> GetAll()
+        {
+            return context.TaskDetail.ToList();
+        }
+        public TaskDetail GetById(int id)
+        {
+            var obj = context.TaskDetail.Find(id);
+            return obj;
+        }
+        public bool Insert(TaskDetail obj)
+        {
+            context.TaskDetail.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(TaskDetail obj)
+        {
+            context.TaskDetail.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.TaskDetail.Find(id);
+            context.TaskDetail.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
