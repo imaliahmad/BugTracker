d930565327b49af92872353a85d5461490b4f37c
MehrozQazi <mehrozqazi346@gmail.com>
2023-06-11 00:25:15 +0500

    EndPoints created on Projects

---
+++ BugTracker.API/Controllers/ProjectsController.cs
@@ -0,0 +1,141 @@
+﻿using BugTracker.API.DTOs.Request;
+using BugTracker.BLL;
+using BugTracker.BOL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectsBs ProjectsBs;
+
+        public ProjectsController(IProjectsBs _ProjectsBs)
+        {
+            ProjectsBs = _ProjectsBs;
+        }
+
+        /// <summary>
+        /// Retrieves all Projects
+        /// </summary>
+        /// <returns>The list of Projects.</returns>
+        [HttpGet]
+        [Route("getAll")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                Projects[] projectsList = ProjectsBs.GetAll().ToArray();
+                List<ProjectsDTO> projectsDTOList = new List<ProjectsDTO>();
+                foreach (var item in projectsList)
+                {
+                    projectsDTOList.Add(ProjectsDTO.ToProjectsDTO(item));
+                }
+
+                return Ok(projectsDTOList.ToArray());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves an projects by ID.
+        /// </summary>
+        /// <param name="id">The ID of the project.</param>
+        /// <returns>The project.</returns>
+        [HttpGet]
+        [Route("getById/{Id}")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                var users = ProjectsBs.GetById(id);
+                ProjectsDTO usersDTO = ProjectsDTO.ToProjectsDTO(users);
+
+                return Ok(usersDTO);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+
+            }
+        }
+
+        /// <summary>
+        /// Creates a new project.
+        /// </summary>
+        /// <param name="orgDTO">The project data.</param>
+        /// <returns>The created project.</returns>
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(ProjectsDTO projectsDTO)
+        {
+            try
+            {
+                Projects Projects = ProjectsBs.Insert(
+                                         ProjectsDTO.ToProjectsModel(projectsDTO)
+                                    );
+
+                projectsDTO = ProjectsDTO.ToProjectsDTO(Projects);
+
+                return Ok(projectsDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Updates an existing project.
+        /// </summary>
+        /// <param name="orgDto">The project data to update.</param>
+        /// <returns>The updated project.</returns>
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update(ProjectsDTO projectsDTO)
+        {
+            try
+            {
+                Projects Projects = ProjectsBs.Update(
+                                         ProjectsDTO.ToProjectsModel(projectsDTO)
+                                    );
+
+                projectsDTO = ProjectsDTO.ToProjectsDTO(Projects);
+
+                return Ok(projectsDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes an project by ID.
+        /// </summary>
+        /// <param name="id">The ID of the project to delete.</param>
+        /// <returns>A boolean indicating whether the deletion was successful.</returns>
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                var users = ProjectsBs.Delete(id);
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ BugTracker.API/DTOs/Request/ProjectsDTO.cs
@@ -0,0 +1,80 @@
+﻿using BugTracker.BOL;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.API.DTOs.Request
+{
+    /// <summary>
+    /// Represents the DTO for Projects.
+    /// </summary>
+    public class ProjectsDTO:BaseEntityDTO
+    {
+        /// <summary>
+        /// Gets or sets the name of the project.
+        /// </summary>
+        [Required(ErrorMessage = "Project Name is required")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization ID (foreign key).
+        /// </summary>
+        public Guid OrgId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date of the project.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end date of the project.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        // Navigations
+
+        /// <summary>
+        /// Gets or sets the organization associated with the project.
+        /// </summary>
+        [ForeignKey("OrgId")]
+        public virtual Organizations? Organizations { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of project users associated with the project.
+        /// </summary>
+        public virtual IEnumerable<ProjectUser>? ProjectUser { get; set; }
+
+        /// <summary>
+        /// Converts an ProjectsDTO object to an Projects model.
+        /// </summary>
+        /// <param name="orgDTO">The ProjecstDTO object to convert.</param>
+        /// <returns>The converted Projects model.</returns>
+        public static Projects ToProjectsModel(ProjectsDTO projectsDTO)
+        {
+            var project = new Projects();
+            project.Id = projectsDTO.Id;
+            project.Name = projectsDTO.Name;
+            project.OrgId = projectsDTO.OrgId;
+            project.StartDate = projectsDTO.StartDate;
+            project.EndDate = projectsDTO.EndDate;
+
+            return project;
+        }
+
+        /// <summary>
+        /// Converts an Projects model to an ProjectsDTO object.
+        /// </summary>
+        /// <param name="model">The Projecst model to convert.</param>
+        /// <returns>The converted ProjectsDTO object.</returns>
+        public static ProjectsDTO ToProjectsDTO(Projects model)
+        {
+            var projectDTO = new ProjectsDTO();
+            projectDTO.Id = model.Id;
+            projectDTO.Name = model.Name;
+            projectDTO.OrgId = model.OrgId;
+            projectDTO.StartDate = model.StartDate;
+            projectDTO.EndDate = model.EndDate;
+
+            return projectDTO;
+        }
+    }
+}
